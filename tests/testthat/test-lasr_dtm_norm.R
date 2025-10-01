ac3d1e50a7fc2554a05ac76e2ebd2e81b768f53d
georgewoolsey <george.woolsey@colostate.edu>
2025-10-01 14:46:28 -0600

    updating some tests

--- tests/testthat/test-lasr_dtm_norm.R
+++ tests/testthat/test-lasr_dtm_norm.R
@@ -1,6 +1,7 @@
 testthat::test_that("lasr_dtm_norm() returns a LASRpipeline", {
   testthat::expect_s3_class(
     lasr_dtm_norm("testtest.tif")
-    , c("LASRpipeline", "list")
+    # , c("LASRpipeline", "list") # lasR 0.13.1 and below
+    , c("PipelinePtr") # lasR at least 0.17.2 maybe earlier
   )
 })
