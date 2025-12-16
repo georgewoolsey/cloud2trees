50105768ded5ca4eca2bc66b2a56e63177c7aede
georgewoolsey <george.woolsey@colostate.edu>
2025-12-16 12:50:57 -0700

    enabling chm input in itd_tuning()

--- R/itd_tuning.R
+++ R/itd_tuning.R
@@ -19,13 +19,14 @@
 #' If providing a custom function, it must always return a numeric value >0 (see examples).
 #' @param min_height numeric. Set the minimum height (m) for individual tree detection
 #' @param chm_res_m numeric. The desired resolution of the CHM produced in meters.
+#' @param input_chm_rast SpatRaster. A SpatRaster class object read with the `terra` package. Can be used instead of defining `input_las_dir`.
 #'
 #' @references
 #' [https://r-lidar.github.io/lidRbook/itd.html](https://r-lidar.github.io/lidRbook/itd.html)
 #'
 #' @return Returns a list with: 1) "plot_samples" is a plot of the sample canopy height model (CHM) and extracted tree crowns for each window size tested;
-#' and 2) "ws_fn_list" is a list of the window size functions tested which can be used to pass the desired function to the `ws`
-#' parameter in [raster2trees()] and/or [cloud2trees()].
+#' and 2) "ws_fn_list" is a list of the window size functions tested which can be used to pass the desired function to the `ws` parameter in [raster2trees()] and/or [cloud2trees()].
+#' and 3) "plot_sample_summary" is a plot summarizing the characteristics of the extracted trees for each window size tested.
 #'
 #' @examples
 #'  \dontrun{
@@ -106,64 +107,44 @@
 #' @export
 #'
 itd_tuning <- function(
-  input_las_dir
+  input_las_dir = NULL
   , n_samples = 3
   , ws_fn_list = NULL
   , min_height = 2
   , chm_res_m = 0.25
+  , input_chm_rast = NULL
 ){
-#####################################################
-# check n_samples
-#####################################################
-  n_samples <- n_samples %>%
-    as.numeric() %>%
-    dplyr::coalesce(3) %>%
-    min(5)
+  if(is.null(input_las_dir) && is.null(input_chm_rast)){stop("`input_las_dir` or `input_chm_rast` must be provided")}
 #####################################################
 # check the las and read as a LASCatalog if exists
 #####################################################
-  las <- check_las_data(input_las_dir)
-  # set the lascatalog options
-  if(inherits(las, "LAScatalog")){
-    lidR::opt_progress(las) <- F
-    lidR::opt_filter(las) <- "-drop_duplicates" ## class 2 = ground; 9 = water; 18 = noise
-    lidR::opt_select(las) <- "xyz" # 0 enables all extra bytes to be loaded...possibly treeID
-    lidR::opt_output_files(las) <- paste0(tempdir(), "/{*}_treed")
+  if(inherits(input_chm_rast, "SpatRaster")) {
+    # get roi
+    roi_poly <- get_rast_extent_poly(input_chm_rast)
+    # roi_poly %>% sf::st_area()
+
+    x_input <- input_chm_rast
+  }else if(!is.null(input_las_dir)){
+    las <- check_las_data(input_las_dir)
+    # set the lascatalog options
+    if(inherits(las, "LAScatalog")){
+      lidR::opt_progress(las) <- F
+      lidR::opt_filter(las) <- "-drop_duplicates" ## class 2 = ground; 9 = water; 18 = noise
+      lidR::opt_select(las) <- "xyz" # 0 enables all extra bytes to be loaded...possibly treeID
+      lidR::opt_output_files(las) <- paste0(tempdir(), "/{*}_treed")
+    }
+    # get roi
+    roi_poly <- las %>% lidR::st_bbox() %>% sf::st_as_sfc()
+    # roi_poly %>% sf::st_area()
+
+    x_input <- las
+  }else{
+    stop("`input_las_dir` or `input_chm_rast` must be provided with proper input type")
   }
 #####################################################
-# bbox based on the points
+# get_sample_pts_poly()
 #####################################################
-  roi_poly <- las %>% lidR::st_bbox() %>% sf::st_as_sfc()
-  # roi_poly %>% sf::st_area()
-  # make the roi 5% smaller to sample from so we don't get sample points on the edge
-  roi_poly <- roi_poly %>%
-    sf::st_buffer(
-      roi_poly %>%
-      sf::st_area() %>%
-      as.numeric() %>%
-      sqrt() %>%
-      `*`(-0.0125) %>%
-      round()
-    )
-  # roi_poly %>% sf::st_area()
-#####################################################
-# data frame of sample points that fall within roi
-#####################################################
-  sample_pts <-
-    # the centroid will always be the first sample point
-    roi_poly %>%
-      sf::st_centroid() %>%
-      sf::st_as_sf() %>%
-      dplyr::bind_rows(
-        sf::st_sample(
-          roi_poly
-          , size = max(5,n_samples*2)
-          , type = "random"
-        ) %>%
-        sf::st_as_sf()
-      ) %>%
-      dplyr::mutate(rnk = dplyr::row_number())
-
+  sample_pts <- get_sample_pts_poly(roi_poly = roi_poly, n_samples = n_samples)
   # ggplot() +
   #   geom_sf(data = roi_poly, fill = NA) +
   #   geom_sf(data = sample_pts, aes(color = rnk)) +
@@ -175,7 +156,7 @@ itd_tuning <- function(
 quiet_sample_las_point_extract_trees <- purrr::quietly(sample_las_point_extract_trees)
 big_ans_list <- 1:nrow(sample_pts) %>%
   purrr::map(\(x) quiet_sample_las_point_extract_trees(
-      las = las
+      x_input = x_input
       , sample_point = sample_pts[x,]
       , ws_fn_list = ws_fn_list
       , min_height = min_height
@@ -281,14 +262,14 @@ if(is.null(big_ans_list) || dplyr::coalesce(length(big_ans_list), 0)<1){
 #################################################################
 #################################################################
 ## intermediate function (actually the workhorse) to:
-  ## clip the las based on a buffered point
-  ## run cloud2trees::cloud2raster() to get chm
+  ## clip the las/rast based on a buffered point
+  ## run cloud2trees::cloud2raster() to get chm if las
   ## run cloud2trees::raster2trees() over a list of ws functions
   ## return chm, crown polys unique by ws fn, and ws functions used
 #################################################################
 #################################################################
 sample_las_point_extract_trees <- function(
-  las # a LASCatalog, maybe a LAS
+  x_input # a LASCatalog, maybe a LAS, def a SpatRaster
   , sample_point = NULL
   , plot_area_m2 = 1000
   , ws_fn_list = NULL
@@ -300,8 +281,10 @@ sample_las_point_extract_trees <- function(
 ) {
   #check las
   if(
-    !inherits(las, "LAScatalog") && !inherits(las,"LAS")
-  ){stop("las must be LAS or LAScatalog")}
+    !inherits(x_input, "LAScatalog")
+    && !inherits(x_input,"LAS")
+    && !inherits(x_input,"SpatRaster")
+  ){stop("x_input must be LAS or LAScatalog or SpatRaster")}
   #####################################################
   # make an empty return object if steps don't work
   #####################################################
@@ -348,11 +331,22 @@ sample_las_point_extract_trees <- function(
   # default sample point to centroid
   ## could also check that the sf objects are POINT...maybe another day
     if(!inherits(sample_point, "sf")){
-      sample_point <- las %>%
-        lidR::st_bbox() %>%
-        sf::st_as_sfc() %>%
-        sf::st_centroid() %>%
-        sf::st_as_sf()
+      # las
+      if(
+        inherits(x_input, "LAScatalog")
+        || inherits(x_input,"LAS")
+      ){
+        sample_point <- x_input %>%
+          lidR::st_bbox() %>%
+          sf::st_as_sfc() %>%
+          sf::st_centroid() %>%
+          sf::st_as_sf()
+      }else{ # this must be a raster
+        sample_point <- x_input %>%
+          get_rast_extent_poly() %>%
+          sf::st_centroid() %>%
+          sf::st_as_sf()
+      }
     }else{
       # just get the first element if someone passes >1
       sample_point <- sample_point[1,]
@@ -386,10 +380,29 @@ sample_las_point_extract_trees <- function(
   #   geom_sf(data = sample_point, aes(color = "point"))
 
   #####################################################
-  # clip and save file to pass to cloud2trees
+  # get CHM raster for sample area
   #####################################################
+  if (inherits(x_input, "SpatRaster")) {
+    ########################
+    # clip
+    ########################
+    chm_rast <- x_input %>%
+      terra::crop(
+        terra::vect(plot_poly)
+      )
+    ########################
+    # min_height
+    ########################
+    chm_rast <- terra::ifel(chm_rast < min_height, NA, chm_rast)
 
-  # clip las
+    if(is.null(chm_rast) || terra::ncell(chm_rast) == 0){
+      return(empty_return)
+    }
+  }else{
+    ########################
+    # clip and save file to pass to cloud2trees
+    ########################
+    # clip las
     # at a minimum we need to set the opt_output_files if ctg
     if(inherits(las, "LAScatalog")){
       lidR::opt_progress(las) <- F
@@ -410,42 +423,47 @@ sample_las_point_extract_trees <- function(
       # we need to write the las
       las_fp <- lidR::writeLAS(clip_las, file = file.path(tempfile(), "clip.las"))
     }else{return(empty_return)}
+    ########################
+    # cloud2trees that stuff to get CHM
+    ########################
+      # get the raster
+      safe_cloud2raster <- purrr::safely(cloud2raster)
+      cloud2raster_ans <- safe_cloud2raster(
+        input_las_dir = las_fp
+        , output_dir = tempdir()
+        , min_height = min_height # 1.37 ## dbh
+        , chm_res_m = chm_res_m
+      )
+      # get the result
+      if(is.null(cloud2raster_ans$error)){
+        cloud2raster_ans <- cloud2raster_ans$result
+        chm_rast <- cloud2raster_ans$chm_rast
+      }else{
+        return(empty_return)
+      }
+      # chm_rast %>% terra::plot()
+  }
 
   #####################################################
-  # cloud2trees that stuff
+  # check for NA
+  # make sure there are plenty filled cells
   #####################################################
-    # get the raster
-    safe_cloud2raster <- purrr::safely(cloud2raster)
-    cloud2raster_ans <- safe_cloud2raster(
-      input_las_dir = las_fp
-      , output_dir = tempdir()
-      , min_height = min_height # 1.37 ## dbh
-      , chm_res_m = chm_res_m
-    )
-    # get the result
-    if(is.null(cloud2raster_ans$error)){
-      cloud2raster_ans <- cloud2raster_ans$result
-    }else{
-      return(empty_return)
-    }
-    # cloud2raster_ans$chm_rast %>% terra::plot()
-
-    # check for NA
-    # make sure there are plenty filled cells
     if(
-      as.numeric(terra::global(cloud2raster_ans$chm_rast, fun = "isNA")) == terra::ncell(cloud2raster_ans$chm_rast) ||
-      as.numeric(terra::global(cloud2raster_ans$chm_rast, fun = "isNA")) >= round(terra::ncell(cloud2raster_ans$chm_rast)*0.98)
+      as.numeric(terra::global(chm_rast, fun = "isNA")) == terra::ncell(chm_rast) ||
+      as.numeric(terra::global(chm_rast, fun = "isNA")) >= round(terra::ncell(chm_rast)*0.98)
     ){
       return(empty_return)
     }
 
-    # map cloud2trees over each ws function
+  #####################################################
+  # map raster2trees over each ws function
+  #####################################################
     safe_raster2trees <- purrr::safely(raster2trees)
     raster2trees_ans_list <-
       ws_fn_list %>%
       purrr::map(\(x)
         safe_raster2trees(
-          chm_rast = cloud2raster_ans$chm_rast
+          chm_rast = chm_rast
           , outfolder = tempdir()
           , min_height = min_height # 1.37 ## dbh
           , ws = x
@@ -478,7 +496,7 @@ sample_las_point_extract_trees <- function(
       # for when we go to patchwork separate samples together
       ggplot2::geom_sf(data = plot_poly, fill = NA, lwd = 0) +
       ggplot2::geom_tile(
-        data = cloud2raster_ans$chm_rast %>%
+        data = chm_rast %>%
           terra::as.data.frame(xy=T) %>%
           dplyr::rename(f=3)
         , mapping = ggplot2::aes(x = x, y = y, fill = f)
@@ -562,14 +580,14 @@ sample_las_point_extract_trees <- function(
         ggplot2::scale_y_continuous(labels = scales::comma, expand = ggplot2::expansion(mult = c(0,0.2))) +
         ggplot2::labs(
           x = "height bin (m)"
-          , y = "# trees", fill = "", color = ""
+          , y = "# trees", fill = ""
           # , subtitle = "Tree Height Distribution"
         ) +
         ggplot2::theme_light() +
         ggplot2::theme(
           legend.position = "top"
           , axis.text.y = ggplot2::element_text(size=6)
-          , axis.text.x = ggplot2::element_text(size=5)
+          , axis.text.x = ggplot2::element_text(size=5, angle = 90)
           , strip.text = ggplot2::element_text(size=7,color = "black")
         )
         # ggplot2::guides(
@@ -584,7 +602,7 @@ sample_las_point_extract_trees <- function(
       sf::st_drop_geometry() %>%
       dplyr::mutate(facet = "Tree Height vs. Crown Diameter") %>%
       ggplot2::ggplot(mapping = ggplot2::aes(x = tree_height_m, y = crown_diameter_m, color = ws_fn, fill = ws_fn)) +
-        ggplot2::geom_jitter(alpha = 0.8, size = 2, width = 0.05, height = 0.05) +
+        ggplot2::geom_jitter(alpha = 0.66, size = 2, width = 0.05, height = 0.05) +
         ggplot2::facet_grid(cols = dplyr::vars(facet)) +
         ggplot2::scale_color_viridis_d(option="viridis") +
         ggplot2::scale_fill_viridis_d(option="viridis") +
@@ -640,7 +658,7 @@ sample_las_point_extract_trees <- function(
         ggplot2::labs(
           x = ""
           , y = ""
-          , fill = "", color = ""
+          , fill = ""
         ) +
         ggplot2::theme_light() +
         ggplot2::theme(
@@ -664,7 +682,7 @@ sample_las_point_extract_trees <- function(
     # return it
     return(list(
       crowns = crowns
-      , chm_rast = cloud2raster_ans$chm_rast
+      , chm_rast = chm_rast
       , plt = plt
       , plt_sum = plt_sum
       , ws_fn_list = ws_fn_list
@@ -691,4 +709,86 @@ parse_raster2trees_ans_list <- function(x,y){
     dplyr::mutate(ws_fn = nm) %>%
     dplyr::relocate(ws_fn)
   return(ans)
+}
+
+#################################################################
+#################################################################
+## intermediate function to:
+  ## get a sample of points for a given polygon extent
+#################################################################
+#################################################################
+get_sample_pts_poly <- function(roi_poly, n_samples) {
+  #####################################################
+  # check n_samples
+  #####################################################
+    n_samples <- n_samples %>%
+      as.numeric() %>%
+      dplyr::coalesce(3) %>%
+      min(5)
+  #####################################################
+  # check roi_poly
+  #####################################################
+  if(!inherits(sf::st_union(roi_poly), "sfc")){stop("could not identify polygon roi data")}
+  # make the roi 5% smaller to sample from so we don't get sample points on the edge
+    roi_poly <- roi_poly %>%
+      sf::st_union() %>%
+      sf::st_buffer(
+        roi_poly %>%
+        sf::st_area() %>%
+        as.numeric() %>%
+        sqrt() %>%
+        `*`(-0.0125) %>%
+        round()
+      )
+  # area?
+    # roi_poly %>% sf::st_area()
+  #####################################################
+  # data frame of sample points that fall within roi
+  #####################################################
+    sample_pts <-
+      # the centroid will always be the first sample point
+      roi_poly %>%
+        sf::st_centroid() %>%
+        sf::st_as_sf() %>%
+        dplyr::bind_rows(
+          sf::st_sample(
+            roi_poly
+            , size = max(5,n_samples*2)
+            , type = "random"
+          ) %>%
+          sf::st_as_sf()
+        ) %>%
+        dplyr::mutate(rnk = dplyr::row_number())
+  # return
+    return(sample_pts)
+}
+
+#################################################################
+#################################################################
+## intermediate function to:
+  ## get an sf polygon object of the non-NA data extent of a SpatRaster
+#################################################################
+#################################################################
+get_rast_extent_poly <- function(rast) {
+  if(!inherits(rast, "SpatRaster")){
+    stop("Input must be a SpatRaster object from the terra package.")
+  }
+
+  # trim the raster to the smallest extent covering non-NA cells
+  trimmed_raster <- terra::trim(rast)
+
+  if(is.null(trimmed_raster) || terra::ncell(trimmed_raster) == 0){
+    stop("raster is entirely NA or empty.")
+  }
+
+  # get the extent of the trimmed raster
+  trimmed_extent <- terra::ext(trimmed_raster)
+
+  # convert the extent object to a bbox
+  bbox_obj <- sf::st_bbox(trimmed_extent, crs = terra::crs(rast))
+
+  # convert the bbox object into an sfc polygon
+  sfc_polygon <- sf::st_as_sfc(bbox_obj) %>% sf::st_union()
+
+  return(sfc_polygon)
 }
